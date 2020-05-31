78d28da50352c30152359a5f8582beef8f36b177
LuisERivas <lucho.rivas.villacorta@gmail.com>
2020-05-31 00:14:31 -0700

    added authentication backend routes

--- server.js
+++ server.js
@@ -2,9 +2,8 @@
 
 const express = require('express')
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const items = require('./routes/api/items')
 const path = require('path')
+const config = require('config')
 
 // initilize express as app
 
@@ -12,21 +11,26 @@ const app = express()
 
 // middle ware
 
-app.use(bodyParser.json())
+app.use(express.json())
 
 // DB Config
-const db = require('./config/keys').mongoURI
+const db = config.get('mongoURI')
 
 // Connect to mongo using mongoose
 
 mongoose
-  .connect(db)
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  })
   .then(() => console.log('Mongo is connected bro!'))
   .catch(err => console.log(err))
 
 // Use Routes
-app.use('/api/items', items)
-
+app.use('/api/items', require('./routes/api/items'))
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
 // serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // set static folder
