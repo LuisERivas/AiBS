49c529b29c23e689c9a240005cffcbd08236131b
LuisERivas <lucho.rivas.villacorta@gmail.com>
2020-06-22 10:05:52 -0700

    added registration

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import AppNavbar from './components/AppNavbar'
@@ -6,18 +6,24 @@ import ShoppingList from './components/ShoppingList'
 import { Provider } from 'react-redux'
 import store from './store'
 import ItemModal from './components/ItemModal'
+import { loadUser } from './actions/authActions'
 
-function App () {
-  return (
-    <Provider store={store}>
+class App extends Component {
+  componentDidMount () {
+    store.dispatch(loadUser())
+  }
+  render () {
+    return (
+      <Provider store={store}>
 
-      <div className='App'>
-        <AppNavbar />
-        <ItemModal />
-        <ShoppingList />
-      </div>
-    </Provider>
-  )
+        <div className='App'>
+          <AppNavbar />
+          <ItemModal />
+          <ShoppingList />
+        </div>
+      </Provider>
+    )
+  }
 }
 
 export default App
